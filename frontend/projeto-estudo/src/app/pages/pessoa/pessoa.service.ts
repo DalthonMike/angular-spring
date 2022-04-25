d3e5259cace421e5115ea1e43afc1b9eaf31e37a
dalthonMike <dalthonmike13@gmail.com>
2022-04-25 10:19:30 -0300

    [FRONTEND] - Criando o método http para listar as pessoas já cadastradas e alterando o método de cadastro

--- frontend/projeto-estudo/src/app/pages/pessoa/pessoa.service.ts
+++ frontend/projeto-estudo/src/app/pages/pessoa/pessoa.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
+import {PessoaModel} from "./model/pessoa.model";
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +18,11 @@ export class PessoaService {
     return "";
   }
 
-  cadastro(formData: FormData) {
+  cadastro(formData: PessoaModel) {
     return this.http.post<any>(`${this.pathBase}/pessoa`, formData, {observe: 'response'});
+  }
+
+  listar() {
+    return this.http.get<any>(`${this.pathBase}/pessoa`)
   }
 }
